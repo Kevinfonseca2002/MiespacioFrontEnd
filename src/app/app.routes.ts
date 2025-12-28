d08cf3155e117308f9be29a76fbb09a1716cbc83
Kevinfonseca2002 <fonsecakevin74@gmail.com>
2025-12-28 11:45:38 -0500

    [~]projects/Font and titles were edited with SCSS

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,13 @@
 import { Routes } from '@angular/router';
 import { ArquitectProfile } from './pages/private/arquitect-profile/arquitect-profile';
+import { Billing } from './pages/private/billing/billing';
+import { Projects } from './pages/private/projects/projects';
+import { Quotation } from './pages/private/quotation/quotation';
 
 export const routes: Routes = [
-  {path:"profile", component: ArquitectProfile}
+  {path:"profile", component: ArquitectProfile},
+  {path: "billing", component: Billing},
+  {path:"project", component: Projects},
+  {path: "requestedQuotes", component: Quotation},
+  {path: "**", redirectTo: "profile"}
 ];
