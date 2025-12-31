8bcc9110938795a361d657e0a14369abbd71b506
Kevinfonseca2002 <fonsecakevin74@gmail.com>
2025-12-31 01:30:21 -0500

    [~]Overall header was created and added inside Project

--- src/app/pages/private/projects/projects.ts
+++ src/app/pages/private/projects/projects.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { Header } from '../../../shared/children/header/header';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  standalone: true,
+  imports: [Header, RouterLink],
   templateUrl: './projects.html',
   styleUrl: './projects.scss',
 })
