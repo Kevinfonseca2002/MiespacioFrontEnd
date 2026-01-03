d3711003cc392aaf8ce2334cf642998033644a59
Kevinfonseca2002 <fonsecakevin74@gmail.com>
2026-01-02 22:22:49 -0500

    [~]arquitect-profile.scss was modified and styles were all added excepting for the mobile

--- src/app/pages/private/arquitect-profile/arquitect-profile.ts
+++ src/app/pages/private/arquitect-profile/arquitect-profile.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Header } from '../../../shared/children/header/header';
 
 @Component({
   selector: 'app-arquitect-profile',
-  imports: [],
+  imports: [CommonModule, Header],
   templateUrl: './arquitect-profile.html',
   styleUrl: './arquitect-profile.scss',
 })
